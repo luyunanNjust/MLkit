1d8a50395b3a2aee251a7a05dbd0a3dc969463f6
Eric <luyunan1904@163.com>
2019-08-07 23:06:58 +0800

    上传属性值连续的高斯贝叶斯分布

--- classifier/naive_bayes.py
+++ classifier/naive_bayes.py
@@ -2,8 +2,7 @@ import numpy as np
 from functools import reduce
 from BaseClassifier import BaseClassifier
 
-
-class DiscreteBayes(BaseClassifier):
+class DiscreteNB(BaseClassifier):
 	"""
 	【适用于属性为离散值的贝叶斯分类器】
 	"""
@@ -12,11 +11,18 @@ class DiscreteBayes(BaseClassifier):
 		self._X_test = None
 		self._y_test = None
 		self._C = None
+		# 外部可访问属性
+		self.class_prior_ = list()
 
 	# 训练学习器
 	def fit(self, X, y):
-		self._C = sorted(list(np.unique(y)))
 		super().fit(X, y)
+		self._C = sorted(list(np.unique(y)))
+		for ci in self._C:
+			Pci = (self._y[self._y == ci]).size / self._y.size
+			self.class_prior_.append(Pci)
+		self.class_prior_ = np.array(self.class_prior_)
+		return self
 	
 	# 预测重用代码
 	def _pred(self, i):
@@ -53,7 +59,9 @@ class DiscreteBayes(BaseClassifier):
 		
 		trans = np.vectorize(trans) # 向量化
 		trans(distri_arr)
-		return np.array(res[1:])
+		res = np.array(res[1:])
+		res = res / res.sum(axis = 1).reshape(-1,1)
+		return res
 	
 	# 预测
 	def predict(self, X):
@@ -65,3 +73,35 @@ class DiscreteBayes(BaseClassifier):
 		get_max = np.vectorize(get_max)
 		return np.array(get_max(distri_arr))
 
+class GaussianNB_(DiscreteNB):
+	"""
+	【适用于属性值连续的贝叶斯分类器】
+	"""
+	def __init__(self):
+		super().__init__()
+		self.gauss_func_dict = dict()
+	
+	def __gauss_distr(self, sigma, miu, x):
+		return np.exp(-np.power(x - miu, 2) / (2*sigma*sigma)) / (sigma * np.sqrt(2*np.pi))
+	
+	def fit(self, X, y):
+		self._C = sorted(list(np.unique(y)))
+		self._X = X
+		self._y = y
+		for ci in self._C:
+			Xci = X[y == ci]
+			gs_func = np.vectorize(lambda i : (Xci[:,i].std(), Xci[:,i].mean()))
+			self.gauss_func_dict[ci] = gs_func(range(Xci.shape[1]))
+			Pci = (self._y[self._y == ci]).size / self._y.size
+			self.class_prior_.append(Pci)
+		self.class_prior_ = np.array(self.class_prior_)
+		return self
+		
+	def _pred(self, i):
+		x = self._X_test[i]
+		C_distribution = dict().fromkeys(self._C)
+		# 计算公式 P(ci|x) = IIP(xd|ci) * P(ci) / P(x)
+		for ci in self._C:
+			p_fea = self.__gauss_distr(self.gauss_func_dict[ci][0], self.gauss_func_dict[ci][1], x)
+			C_distribution[ci] = reduce(lambda x,y:x*y, p_fea) * self.class_prior_[ci - self._C[0]]
+		return C_distribution
